9b30b2eac867e4165450c177b53abfcddbec1c00
beisenbek.baisakov <beisenbek.baisakov@yandex.ru>
2017-03-18 12:38:56 +0600

    init

--- DelegateSample/Program.cs
+++ DelegateSample/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace DelegateSample
 {
+    delegate void PrintDelegate();
+
+
+    class SuperClass
+    {
+        public void PrintOK()
+        {
+            Console.WriteLine("ok");
+        }
+    }
+
+    class SuperClass2
+    {
+        public void PrintOK()
+        {
+            Console.WriteLine("ok2");
+        }
+    }
+
+    class SuperClass3
+    {
+        public void PrintOK()
+        {
+            Console.WriteLine("ok3");
+        }
+    }
+
+
+    class SimpleClass
+    {
+        public PrintDelegate pd;
+        public void PrintOK()
+        {
+            pd.Invoke();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            SuperClass c = new SuperClass();
+            SuperClass2 c2 = new SuperClass2();
+            SuperClass3 c3 = new SuperClass3();
+            SimpleClass s = new SimpleClass();
+            s.pd = c3.PrintOK;
+            s.PrintOK();
         }
     }
 }
